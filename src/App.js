4eb98abc9d47c21131b545a43cc30af36c26f107
Henrique Garcia <https.henriquegarcia@gmail.com>
2022-07-07 01:52:07 -0300

    Implementação do contexto de temas do Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './Utils/globals'
 
+import { theme } from './Utils/theme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
     
-    </>
+      
+
+    </ThemeProvider>
   )
 }
 
